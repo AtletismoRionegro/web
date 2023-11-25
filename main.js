0cd45fb046fc74a21317360333b8b5621e1e7016
Samuel Alvarez <samualvarez34@gmail.com>
2023-11-25 00:57:38 -0500

    responsive fix and hosting images

---
+++ main.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+    let x = document.getElementById("navbar");
+    if (x.className === "nav-items") {
+        x.className += " responsive";
+    } else {
+        x.className = "nav-items"
+    }
+}
